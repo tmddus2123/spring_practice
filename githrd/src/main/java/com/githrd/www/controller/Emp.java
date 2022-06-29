ccfd75bdebd777328dc510a855743393fe621227
tmddus2123 <tmddus2123@gmail.com>
2022-06-29 16:15:07 +0900

    22.06.29

--- githrd/src/main/java/com/githrd/www/controller/Emp.java
+++ githrd/src/main/java/com/githrd/www/controller/Emp.java
@@ -23,8 +23,10 @@ public class Emp {
 		list.add("부 서");
 		list.add("직 급");
 		
+		List<EmpVO> iList = eDao.getInitLIst();
 		// 데이터 전달하고
 		mv.addObject("LIST", list);
+		mv.addObject("ILIST",iList);
 		mv.setViewName("emp/empPage");
 		return mv;
 	}
@@ -42,6 +44,22 @@ public class Emp {
 	@ResponseBody
 	public List<EmpVO> partList(EmpVO eVO){
 		List<EmpVO> list = eDao.partList(eVO);
+		return list;
+	}
+	
+	// 사원 정보 조회 요청 처리함수
+	@RequestMapping("/empInfo.blp")
+	@ResponseBody
+	public EmpVO empInfo(EmpVO eVO) {
+		eVO = eDao.getInfo(eVO);
+		return eVO;
+	}
+	
+	// 이니셜로 이름 리스트 조회 요청 처리함수
+	@RequestMapping("/enameList.blp")
+	@ResponseBody
+	public List<EmpVO> enameList(EmpVO eVO){
+		List<EmpVO> list = eDao.nameList(eVO);
 		return list;
 	}
 }
